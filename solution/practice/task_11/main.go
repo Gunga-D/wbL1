10a19c42203b75dbc84a5adbb350396f91370fb4
Gunga-D <dondokov.gunga@mail.ru>
2022-08-19 23:52:27 +0300

    make practice task 11

---
+++ solution/practice/task_11/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstSet := map[int]struct{}{
+		2:  struct{}{},
+		3:  struct{}{},
+		8:  struct{}{},
+		21: struct{}{},
+	}
+	secondSet := map[int]struct{}{
+		8: struct{}{},
+		6: struct{}{},
+		3: struct{}{},
+		1: struct{}{},
+	}
+
+	intesection := []int{}
+	for value, _ := range firstSet {
+		_, found := secondSet[value]
+		if found {
+			intesection = append(intesection, value)
+		}
+	}
+	fmt.Println("Intersection", intesection)
+}
